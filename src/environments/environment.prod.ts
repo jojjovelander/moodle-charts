f7c011836400885dfc934a159820a94748ecd461
Johanna Velander <johanna@adhibit.se>
2020-05-14 08:49:20 +0200

    Add host and token to prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  TOKEN: 'c5f7150439c23ef073b1ae67724a9762',
+  HOST: 'http://localhost:8000/webservice/rest/server.php?wstoken=',
 };

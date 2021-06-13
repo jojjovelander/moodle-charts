77b46f16a1f8b653f44641e2389477fce53c9982
Johanna Velander <johanna@adhibit.se>
2021-06-13 22:11:54 +0200

    Add moustaching

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  TOKEN: 'c5f7150439c23ef073b1ae67724a9762',
-  HOST: 'http://localhost:8000/webservice/rest/server.php?wstoken=',
+  TOKEN: '{{ moodle_webservice_api_key }}',
+  HOST: '{{ angular_resources_location }}',
 };
